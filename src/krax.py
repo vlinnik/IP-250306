14084f964dcd337fcc740aa2eea9b1d756463e42
Linnik V.V <vlinnik@mail.ru>
2025-03-26 08:21:14 +0300

    Исправление ошибок
    - Вибраторы дозатора цемента и бункеров

--- src/krax.py
+++ src/krax.py
@@ -2,6 +2,7 @@ from pyplc.config import plc
 from project import name as project_name, version_short as project_version
 from concrete import Factory,Weight,Container,Lock,Dosator,Mixer,MSGate,Motor,Manager,Readiness,Loaded,FlowMeter
 from concrete.elevator import ElevatorGeneric
+from concrete.vibrator import UnloadHelper,Vibrator
 from concrete.imitation import iMOTOR,iGATE,iWEIGHT,iVALVE,iELEVATOR,iROTARYFLOW
 from pyplc.utils.misc import BLINK
 import sys
@@ -13,7 +14,7 @@ factory_1 = Factory()
 cement_m_1 = Weight(raw = plc.CEMENT_M_1, mmax = 1500)
 silage_1 = Container( m = lambda: cement_m_1.m, out = plc.AUGER_ON_1, lock = Lock(key=~plc.DCEMENT_CLOSED_1) )
 dcement_1 = Dosator( m = lambda: cement_m_1.m, closed = plc.DCEMENT_CLOSED_1, out = plc.DCEMENT_OPEN_1, lock = Lock(key=lambda: plc.AUGER_ON_1 or not plc.MIXER_ISON_1 ),containers=[silage_1] )
-
+dc_vibrator_1 = UnloadHelper(q=plc.DC_VIBRATOR_ON_1,dosator=dcement_1,weight=cement_m_1)
 aerator_1 = BLINK(enable=plc.AUGER_ON_1, q = plc.AERATOR_ON_1, t_off = 3000 )
 
 water_m_1 = Weight(raw = plc.WATER_M_1, mmax = 500)
@@ -26,7 +27,8 @@ fillers_m_1 = Weight(raw=plc.CONVEYOR_M_1, mmax = 8000)
 filler_1 = Container(m = lambda: fillers_m_1.m, out=plc.FILLER_OPEN_1, lock = Lock(key=lambda: plc.FILLER_OPEN_2 or plc.CONVEYOR_ON_1 ))
 filler_2 = Container(m = lambda: fillers_m_1.m, out=plc.FILLER_OPEN_2, lock = Lock(key=lambda: plc.FILLER_OPEN_1 or plc.CONVEYOR_ON_1 ))
 dfillers_1 = Dosator(m = lambda: fillers_m_1.m, out=plc.CONVEYOR_ON_1, containers=[filler_1,filler_2],lock=Lock(key=lambda: plc.FILLER_OPEN_1 or plc.FILLER_OPEN_2 or not plc.ELEVATOR_BELOW_1 ))
-
+vibrator_1 = Vibrator(q=plc.VIBRATOR_ON_1,containers=[plc.FILLER_OPEN_1],weight=fillers_m_1)
+vibrator_2 = Vibrator(q=plc.VIBRATOR_ON_2,containers=[plc.FILLER_OPEN_2],weight=fillers_m_1)
 elevator_1 = ElevatorGeneric(above=plc.ELEVATOR_ABOVE_1,below = plc.ELEVATOR_BELOW_1, middle = plc.ELEVATOR_MIDDLE_1,up = plc.ELEVATOR_UP_1, down=plc.ELEVATOR_DOWN_1, containers = [filler_1,filler_2],dosator = dfillers_1)
 
 gate_1 = MSGate( closed = plc.MIXER_CLOSED_1, opened = plc.MIXER_OPENED_1,open=plc.MIXER_OPEN_1)
@@ -51,7 +53,7 @@ ready_1 = Readiness([dcement_1,dwater_1,elevator_1]) #что должно быт
 loaded_1 = Loaded([dcement_1,dwater_1,elevator_1,addition_1])  #что должно быть unloaded чтобы смеситель считать загруженным
 manager_1 = Manager(collected=ready_1,loaded=loaded_1,mixer=mixer_1,dosators=[dcement_1,dwater_1,dfillers_1,elevator_1],loadOrder= loadOrder_1)  #dosators= кому надо установить unload для начала загрузки
 
-instances = [ factory_1,gate_1,motor_1,mixer_1,cement_m_1,silage_1,dcement_1,water_m_1,water_1,dwater_1, fillers_m_1,filler_1,filler_2,dfillers_1,elevator_1,ready_1,loaded_1,manager_1,addition_1,aerator_1 ]
+instances = [ factory_1,gate_1,motor_1,mixer_1,cement_m_1,silage_1,dcement_1,water_m_1,water_1,dwater_1, fillers_m_1,filler_1,filler_2,dfillers_1,elevator_1,ready_1,loaded_1,manager_1,addition_1,aerator_1,vibrator_1,vibrator_2,dc_vibrator_1 ]
 
 if sys.platform=='linux':
   if sys.platform=='linux':
